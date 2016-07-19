3064dbb408abe85d7deb579257b111f62cbde5a4
goddessting <1766153116@qq.com>
2016-07-19 11:44:51 +0800

    completely buildReceiptItems()

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,23 +1,46 @@
 'use strict';
 
-function  buildCartItems(tags, allItems){
+function buildCartItems(tags, allItems) {
   let cartItems = [];
 
-  for(let tag of tags){
+  for (let tag of tags) {
     let splittedTag = tag.split('-');
     let barcode = splittedTag[0];
     let count = parseFloat(splittedTag[1] || 1);
 
     let cartItem = cartItems.find(cartItem => cartItem.item.barcode === barcode);
 
-    if(cartItem){
+    if (cartItem) {
       cartItem.count += count;
-    }else{
+    } else {
       let item = allItems.find(item => item.barcode === barcode);
 
-      cartItems.push({item:item, count: count});
+      cartItems.push({item: item, count: count});
     }
   }
 
   return cartItems;
+}
+
+function buildReceiptItems(cartItems, promotions) {
+  let receiptItems = [];
+  let barcodes = promotions[0].barcodes;
+
+  for (let cartItem of cartItems) {
+      let barcode = barcodes.find(barcode => cartItem.item.barcode === barcode);
+
+      if (barcode && cartItem.count >= 2) {
+        let cartItem = cartItems.find(cartItem => cartItem.item.barcode === barcode);
+
+        receiptItems.push({
+          cartItem: cartItem,
+          subtotal: cartItem.count * cartItem.item.price,
+          saved: cartItem.count / 2 * cartItem.item.price
+        });
+      }else{
+        receiptItems.push({cartItem: cartItem, subtotal: cartItem.count * cartItem.item.price, saved: 0.00});
+      }
+  }
+
+  return receiptItems;
 }
--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -129,21 +129,100 @@ describe('pos', () => {
     });
   });
 
-//  it('should print correct text', () => {
-//
-//    spyOn(console, 'log');
-//
-//    printReceipt(inputs);
-//
-//    const expectText = `***<没钱赚商店>收据***
-//名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
-//名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)
-//名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
-//----------------------
-//总计：51.00(元)
-//节省：7.50(元)
-//**********************`;
-//
-//    expect(console.log).toHaveBeenCalledWith(expectText);
-//  });
+  describe('receiptItems', () => {
+    let promotions;
+
+    beforeEach(() => {
+      promotions = [
+        {
+          type: 'BUY_TWO_GET_ONE_FREE',
+          barcodes: [
+            'ITEM000000',
+            'ITEM000001',
+            'ITEM000005'
+          ]
+        }
+      ];
+    });
+
+    it('when cartItems can discount', () => {
+      let cartItems = [{
+        item: {
+          barcode: 'ITEM000001',
+          name: '雪碧',
+          unit: '瓶',
+          price: 3.00
+        },
+        count: 2
+      }];
+
+      let receiptItems = buildReceiptItems(cartItems, promotions);
+      let expectReceiptItems = [{
+        cartItem: {
+          item: {
+            barcode: 'ITEM000001',
+            name: '雪碧',
+            unit: '瓶',
+            price: 3.00
+          },
+          count: 2
+        },
+        subtotal: 6.00,
+        saved: 3.00
+      }];
+
+      expect(receiptItems).toEqual(expectReceiptItems);
+    });
+
+    it('when cartItems no discount', () => {
+      let cartItems = [
+        {
+          item: {
+            barcode: 'ITEM000002',
+            name: '苹果',
+            unit: '斤',
+            price: 5.50
+          },
+          count: 3
+        }];
+
+      let receiptItems = buildReceiptItems(cartItems, promotions);
+
+      let expectReceiptItems = [
+        {
+          cartItem: {
+            item: {
+              barcode: 'ITEM000002',
+              name: '苹果',
+              unit: '斤',
+              price: 5.50
+            },
+            count: 3
+          },
+          subtotal: 16.5,
+          saved: 0.00
+        }];
+
+      expect(receiptItems).toEqual(expectReceiptItems);
+    });
+  });
+
+
+  it('should print correct text', () => {
+
+    spyOn(console, 'log');
+
+    printReceipt(inputs);
+
+    const expectText = `***<没钱赚商店>收据***
+名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)
+名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+----------------------
+总计：51.00(元)
+节省：7.50(元)
+**********************`;
+
+    expect(console.log).toHaveBeenCalledWith(expectText);
+  });
 });
